9e604109783c57e3076a5aba151909fa13c0234e
Aleks Gomeniuk <agsfinks@gmail.com>
2025-10-26 07:55:07 +0000

    feat: read file and print it content to console

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { access, readdir, readFile } from 'fs/promises';
+import path from 'path';
+
 const read = async () => {
-  // Write your code here
+  const filesFolderPath = path.join(process.cwd(), 'files');
+  const fileToReadPath = path.join(filesFolderPath, 'fileToRead.txt');
+  try {
+    await access(fileToReadPath);
+    const fileData = await readFile(fileToReadPath, { encoding: 'utf-8' });
+    console.log(fileData);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();

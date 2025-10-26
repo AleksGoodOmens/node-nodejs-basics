f14722db5073af50fe5937e04754e3393179cb5d
Aleks Gomeniuk <agsfinks@gmail.com>
2025-10-26 08:09:11 +0000

    revert: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,24 +1,5 @@
-import { access, mkdir, readdir, copyFile } from 'fs/promises';
-import path from 'path';
-
-const initialFolderPath = path.join(process.cwd(), 'files');
-const copiedFolderPath = path.join(process.cwd(), 'files_copy');
-
 const copy = async () => {
-  try {
-    await access(initialFolderPath);
-
-    try {
-      await access(copiedFolderPath);
-      throw new Error('FS operation failed');
-    } catch {
-      await mkdir(copiedFolderPath);
-      const paths = await readdir(initialFolderPath);
-      paths.map((file) => copyFile(`${initialFolderPath}/${file}`, `${copiedFolderPath}/${file}`));
-    }
-  } catch {
-    throw new Error('FS operation failed');
-  }
+  // Write your code here
 };
 
 await copy();

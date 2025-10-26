4e0b4749eab417f586c9e4dedb9714beafe46a4f
Aleks Gomeniuk <agsfinks@gmail.com>
2025-10-26 07:34:35 +0000

    feat: rename funvtion

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import { access, rename as syncRename } from 'fs/promises';
+import path from 'path';
+
 const rename = async () => {
-  // Write your code here
+  const oldFilePath = path.join(process.cwd(), 'files/wrongFilename.txt');
+  const newFilePath = path.join(process.cwd(), 'files/properFilename.md');
+
+  try {
+    try {
+      await access(oldFilePath);
+    } catch (error) {
+      throw new Error('FS operation failed');
+    }
+
+    try {
+      await access(newFilePath);
+      throw new Error('FS operation failed');
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+    await syncRename(oldFilePath, newFilePath);
+  } catch (error) {
+    if (error.message === 'FS operation failed') {
+      throw error;
+    }
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();

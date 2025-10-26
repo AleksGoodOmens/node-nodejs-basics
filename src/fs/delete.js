9243e64ecdca404580a710aa84475882390f7f4f
Aleks Gomeniuk <agsfinks@gmail.com>
2025-10-26 07:41:43 +0000

    feat: remove func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { access, rm } from 'fs/promises';
+import path from 'path';
+
 const remove = async () => {
-  // Write your code here
+  const oldFilePath = path.join(process.cwd(), 'files/fileToRemove.txt');
+
+  try {
+    await access(oldFilePath);
+    rm(oldFilePath);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();

2767ef660de6a9749a2423f89aa2ef7c8cdea917
Aleks Gomeniuk <agsfinks@gmail.com>
2025-10-26 08:08:23 +0000

    revert: list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,15 +1,5 @@
-import { access, readdir } from 'fs/promises';
-import path from 'path';
-
 const list = async () => {
-  const filesFolderPath = path.join(process.cwd(), 'files');
-  try {
-    await access(filesFolderPath);
-    const files = await readdir(filesFolderPath);
-    files.forEach((file) => console.log(file));
-  } catch {
-    throw new Error('FS operation failed');
-  }
+  // Write your code here
 };
 
 await list();

86314b131bdd5e26166bcb510e1c0468400cf567
Aleks Gomeniuk <agsfinks@gmail.com>
2025-10-26 08:09:52 +0000

    revert: delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,15 +1,5 @@
-import { access, rm } from 'fs/promises';
-import path from 'path';
-
 const remove = async () => {
-  const oldFilePath = path.join(process.cwd(), 'files/fileToRemove.txt');
-
-  try {
-    await access(oldFilePath);
-    rm(oldFilePath);
-  } catch (error) {
-    throw new Error('FS operation failed');
-  }
+  // Write your code here
 };
 
 await remove();
